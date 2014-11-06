f4461df5d17d9608935ef2a5be067490f2ac5a0c
Charanjit Singh <ckhabra007@gmail.com>
2014-11-06 14:30:08 +0530

    Trying to integerate crash-watcher into nucleus so that it won't need to be called as a deploy process

--- server/crash_watcher.js
+++ server/crash_watcher.js
@@ -1,5 +1,6 @@
 var fs = Npm.require("fs"),
-    path = Npm.require("path");
+    path = Npm.require("path"),
+    spawn = Npm.require("child_process").spawn;
 
 CrashWatcher = {
   initialize: function() {
@@ -18,7 +19,7 @@ CrashWatcher = {
         var files_to_refresh = data.trim() && data.split("\n");
         _.each(files_to_refresh, function(file) {
           Meteor.call("nucleusSetupFileForEditting", file, true, function(err, res) {
-//            FlashMessages.sendError("We had to revert a file.");
+            //            FlashMessages.sendError("We had to revert a file.");
           });
         });
         fs.unlink(refresh_file);
@@ -26,3 +27,36 @@ CrashWatcher = {
     }));
   }
 };
+
+Meteor.startup(function() {
+  console.log(Nucleus.config);
+  var app_dir = "./",
+      app_port = 'localhost:3000';
+
+  //let's launch nuc-watch-meteor from inside the nucleus instead of explicitly calling it
+  var watcher = spawn("nuc-watch-meteor", ["", "-d "+app_dir, "-u "+app_port], {detatched: true});
+
+  watcher.stdout.setEncoding("utf-8");
+  watcher.stderr.setEncoding("utf-8");
+
+  var errHandler = function(err) {
+    console.log("nuc-watch-meteor ERROR", err.message);
+  };
+
+  watcher.stdout.on('err', errHandler);
+  watcher.stderr.on('err', errHandler);
+
+  watcher.stdout.on('data', function(data) {
+    console.log("Nuc-Watch-Meteor: [stdout]", data);
+  });
+
+  watcher.stderr.on('data', function(data) {
+    console.log("Nuc-Watch-Meteor: [strerr]", data);
+  });
+
+  watcher.on('close', function(code) {
+    console.log("nuc-watch-meteor ended with code", code);
+  });
+
+  watcher.unref();
+});
