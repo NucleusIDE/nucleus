593bde60ab3b59b89577fc746b3636c8d8081d1c
Charanjit Singh <ckhabra007@gmail.com>
2014-11-07 15:25:15 +0530

    Spawn nuc-watch-meteor when initializing crash watcher

--- server/crash_watcher.js
+++ server/crash_watcher.js
@@ -3,7 +3,7 @@ var fs = Npm.require("fs"),
     spawn = Npm.require("child_process").spawn;
 
 CrashWatcher = {
-  initialize: function() {
+  setup_crashing_changes_reverter: function() {
     var refresh_file = path.resolve(Nucleus.config.projectDir, ".nuc-files-to-refresh");
     fs.watchFile(refresh_file, Meteor.bindEnvironment(function(curr, prev) {
       if (curr.nlink === 0) {
@@ -25,38 +25,40 @@ CrashWatcher = {
         fs.unlink(refresh_file);
       }));
     }));
+  },
+  spawn_app_crash_watcher: function() {
+    var app_dir = Nucleus.console.projectDir,
+        app_url = 'localhost:3000';
+
+    //let's launch nuc-watch-meteor from inside the nucleus instead of explicitly calling it
+    var watcher = spawn("nuc-watch-meteor", ["", "-d ",app_dir, "-u ",app_url], {detatched: true});
+
+    watcher.stdout.setEncoding("utf-8");
+    watcher.stderr.setEncoding("utf-8");
+
+    var errHandler = function(err) {
+      console.log("nuc-watch-meteor ERROR", err.message);
+    };
+
+    watcher.stdout.on('err', errHandler);
+    watcher.stderr.on('err', errHandler);
+
+    watcher.stdout.on('data', function(data) {
+      console.log("Nuc-Watch-Meteor: [stdout]", data);
+    });
+
+    watcher.stderr.on('data', function(data) {
+      console.log("Nuc-Watch-Meteor: [strerr]", data);
+    });
+
+    watcher.on('close', function(code) {
+      console.log("nuc-watch-meteor ended with code", code);
+    });
+
+    watcher.unref();
+  },
+  initialize: function() {
+    this.setup_crashing_changes_reverter();
+    this.spawn_app_crash_watcher();
   }
 };
-
-Meteor.startup(function() {
-  console.log(Nucleus.config);
-  var app_dir = "./",
-      app_port = 'localhost:3000';
-
-  //let's launch nuc-watch-meteor from inside the nucleus instead of explicitly calling it
-  var watcher = spawn("nuc-watch-meteor", ["", "-d "+app_dir, "-u "+app_port], {detatched: true});
-
-  watcher.stdout.setEncoding("utf-8");
-  watcher.stderr.setEncoding("utf-8");
-
-  var errHandler = function(err) {
-    console.log("nuc-watch-meteor ERROR", err.message);
-  };
-
-  watcher.stdout.on('err', errHandler);
-  watcher.stderr.on('err', errHandler);
-
-  watcher.stdout.on('data', function(data) {
-    console.log("Nuc-Watch-Meteor: [stdout]", data);
-  });
-
-  watcher.stderr.on('data', function(data) {
-    console.log("Nuc-Watch-Meteor: [strerr]", data);
-  });
-
-  watcher.on('close', function(code) {
-    console.log("nuc-watch-meteor ended with code", code);
-  });
-
-  watcher.unref();
-});
