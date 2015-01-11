b3211aaf2dd93583ec970f5b2b665ce855265382
James Gillmore <james@faceyspacey.com>
2015-01-10 23:35:54 -0800

    sdf

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 
 Npm.depends({
   'fibers': '1.0.2',
-  'nucleus-watch-meteor': 'https://github.com/NucleusIDE/nucleus-watch-meteor/archive/7f9155924cc07ce89ba3710c6ad76caca6d19783.tar.gz'
+  'nuc-watch-meteor': 'https://github.com/NucleusIDE/nucleus-watch-meteor/archive/7f9155924cc07ce89ba3710c6ad76caca6d19783.tar.gz'
 });
 
 Package.on_use(function (api, where) {
