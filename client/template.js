ee27d7b10d1c542302e86360f9313a60ad80b923
Charanjit Singh <ckhabra007@gmail.com>
2015-03-16 18:05:09 +0530

    Show a notification on incoming video call.
    Instead of just showing the video film on side, when an incoming call is
    present, make the video call button glow green as a notification.
    
    Many things about webrtc have changed in this commit. We initialize the
    webrtc only when user clicks the video button. Everybody joins the same
    room, so users may leave the room, stop the chat, refresh page or
    whatever, the video chat is always available. All they need to do is
    click the video button and they see the video film on the right.
    
    A new system has been put in place for sending broadcast messages in
    webrtc package using meteor (mongo collection). I tried a lot to get it
    done with sockets of the signaling server, but it interfered with one or
    another feature of the webrtc video chat. So instead I used Meteor's
    collections. To make sure this won't affect performance, we publish and
    subscribe to 30 messages at most and only to messages at most 5 minutes
    old. This is an overkill for performance though, but this is all we
    need. It adds additional benefit that we don't need to initialize webrtc
    until and unless we need it. Not that initializing it had any
    performance implication, but one less thing in memory is good imo.

--- client/template.js
+++ client/template.js
@@ -486,6 +486,20 @@ Template.nucleus_video_chat_film.helpers({
   }
 });
 
+Template.nucleus_video_chat_controls.rendered = function() {
+  MeteorWebrtc.on('newVideoCall', function(message) {
+    console.log(message);
+    if (message.sender === NucleusUser.me().nick) {
+      return;
+    }
+
+    Session.set('incoming_call_recieved', false);
+  });
+};
+
+//Autorun for starting/recieving/stopping a call.
+//It initializes the SimpleWebrtc and create/join/stop a video call
+//depending on Session.get('webrtc-chat-active')
 Tracker.autorun(function() {
   var chatActive = Session.get('webrtc-chat-active');
 
@@ -507,6 +521,15 @@ Tracker.autorun(function() {
 
       window.nucleucWebrtc.on('readyToCall', function () {
         window.nucleucWebrtc.joinRoom('nucleus_room');
+
+        //broadcast message only if it is not recieving a call.
+        //Session.get('incoming_call_recieved') is set to false when an incoming
+        //call has arrived. Otherwise it is true or undefined
+        if (Session.get('incoming_call_recieved') !== false) {
+          MeteorWebrtc.broadcast('newVideoCall', {sender: NucleusUser.me().nick});
+        }
+
+        Session.set('incoming_call_recieved', true);
       });
     }, 100);
   } else {
@@ -520,6 +543,21 @@ Tracker.autorun(function() {
   }
 });
 
+//Autorun for dismissing/showing incoming call notification
+//depending on Session.get('incoming_call_recieved')
+Tracker.autorun(function() {
+  var callRecieved = Session.get('incoming_call_recieved');
+
+  if (typeof callRecieved === 'undefined')
+    return;
+
+  if (callRecieved == false) {
+    $('#start-video-chat').addClass('incoming-call');
+  }
+  if (callRecieved == true) {
+    $('#start-video-chat').removeClass('incoming-call');
+  }
+});
 
 ////////////////////////
 // END VIDEO CONTROLS //
