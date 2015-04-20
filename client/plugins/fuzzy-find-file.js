dde9f8e652dc2881225986df51ec3ccc8a0fb630
Charanjit Singh <ckhabra007@gmail.com>
2015-04-20 16:33:34 +0530

    Use better fuzzy-find algorithm

--- client/plugins/fuzzy-find-file.js
+++ client/plugins/fuzzy-find-file.js
@@ -38,9 +38,40 @@ FuzzyFindFile.prototype.fuzzyFind = function(term) {
   if (!this.fileList || !term)
     return false;
 
+  function fuzzy_match(text, search) {
+    /*
+     Parameter text is a title, search is the user's search
+     */
+    // remove spaces, lower case the search so the search
+    // is case insensitive
+    var search = search.replace(/\ /g, '').toLowerCase();
+    var tokens = [];
+    var search_position = 0;
+
+    // Go through each character in the text
+    for (var n=0; n<text.length; n++)
+    {
+      var text_char = text[n];
+      // if we match a character in the search, highlight it
+      if(search_position < search.length &&
+         text_char.toLowerCase() == search[search_position])
+      {
+        // text_char = '<b>' + text_char + '</b>';
+        search_position += 1;
+      }
+      tokens.push(text_char);
+    }
+    // If are characters remaining in the search text,
+    // return an empty string to indicate no match
+    if (search_position != search.length)
+    {
+      return '';
+    }
+    return tokens.join('');
+  }
+
   return this.fileList.filter(function(text) {
-    var pattern = term.replace(/\s+/g, '').split('').join('.*');
-    return text.match(new RegExp(pattern, 'i'));
+    return fuzzy_match(text, term);
   }).map(function(filepath) {
     return {display: filepath.split('/').slice(-4).join('/'), value: filepath};
   });
