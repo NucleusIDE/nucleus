24e8d2c05c9b50dc719e2933882c99e26e9e679c
Charanjit Singh <ckhabra007@gmail.com>
2015-06-05 20:27:57 +0530

    Add events for working files
    - Edit the file being clicked
    - Remove file from working files when the `X` icon is clicked

--- client/ui/working-files-explorer.js
+++ client/ui/working-files-explorer.js
@@ -3,19 +3,40 @@ var workingFilesState = new ReactiveDict();
 window.GlobalState = GlobalState;
 GlobalState.set('workingFiles', []);
 
-Template.nucleusSidebarExplore.helpers({
+Template.nucleusWorkingFilesExplorer.helpers({
   workingFiles: function() {
     var workingFiles = GlobalState.get('workingFiles');
     return workingFiles.map(function(row) {
       return {
+        id: row.id,
         filename: row.name,
         filepath: row.appPath,
         labelClasses: 'working-files-item-label',
         subcontentClasses: "working-files-item",
         actions: [{
+          id: row.id,
           actionLabelClasses: "nucleus-icon nucleus-icon-close-file"
         }]
       };
     });
-  }
+  },
 });
+
+Template.nucleusWorkingFilesExplorer.events = {
+  "click .nucleus-tree__row": function(e) {
+    Session.set('nucleus_selected_file', this.id);
+  },
+  "click .nucleus-actionsbar__item": function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var row = this;
+    var workingFiles = GlobalState.get('workingFiles');
+
+    workingFiles = _.filter(workingFiles, function(file) {
+      return file.id !== row.id;
+    });
+
+    GlobalState.set('workingFiles', workingFiles);
+  }
+};
