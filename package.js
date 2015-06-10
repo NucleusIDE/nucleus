467c76da52e5fa4941db53c682eb7f6fff18f6f2
Charanjit Singh <ckhabra007@gmail.com>
2015-06-10 15:38:36 +0530

    Fix: Change order of loading git.js and nucleus.js

--- package.js
+++ package.js
@@ -91,10 +91,10 @@ Package.on_use(function (api, where) {
   api.add_files([
     'server/publishers.js',
     'server/login-routes.js',
-    'server/git.js',
     'server/crash_watcher.js',
     'server/nucleus.js',
     'server/files.js',
+    'server/git.js',
     'server/methods.js',
     'server/keepalive.js',
     'server/permissions.js',
