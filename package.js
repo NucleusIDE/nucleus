3c2c8241cadbc7015d6a39705a8c6d24962219e3
Charanjit Singh <ckhabra007@gmail.com>
2015-08-12 23:23:29 +0530

    Version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Realtime Collaborative IDE for Meteor",
-  version: "0.3.0",
+  version: "0.3.1",
   name: "ultimateide:ultimate",
   git: 'https://github.com/nucleuside/nucleus'
 });
